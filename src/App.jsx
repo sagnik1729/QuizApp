3c90da4cf6a4ca0c6d21c604f99c1f51fd48fa44
Sagnik Banerjee <{imsagnik1729@gmail.com}>
2025-05-05 14:40:10 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,9 @@ export default function App() {
           questions.length > 0 && (
             <div className="max-w-md mx-auto mt-8">
               {/** Show either QuestionCard or ScoreDisplay */}
-              {/** currentIdx is managed inside QuizContext */}
-              <QuestionCard />
-              {/* In your reducer, once currentIdx === questions.length, you could conditionally render ScoreDisplay instead */}
-              {/* e.g. in QuizContext you could expose currentIdx and render here: */}
-              {/* state.currentIdx < questions.length ? <QuestionCard /> : <ScoreDisplay /> */}
+              {({ currentIdx }) => (
+                currentIdx < questions.length ? <QuestionCard /> : <ScoreDisplay />
+              )}
             </div>
           )
         )}
